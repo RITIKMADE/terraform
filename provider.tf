d0b932748c21ad8f25ab8cd2b2c2dfe23a614793
RITIKMADE <ritikmade007@gmail.com>
2023-05-03 13:51:45 +0530

    these are files of tf

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA2D4HW52SWD5NPCP7"
+  secret_key = "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"
+  region     = "us-east-1"
+}
---
+++ variable.tf
@@ -0,0 +1,65 @@
+variable "vpc_cidr" {
+
+}
+variable "name" {
+
+}
+variable "az1" {
+
+}
+
+variable "az2" {
+
+}
+
+variable "private_subnet_a_cidr" {
+
+}
+
+variable "private_subnet_b_cidr" {
+
+}
+
+variable "public_subnet_a_cidr" {
+
+}
+variable "public_subnet_b_cidr" {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
